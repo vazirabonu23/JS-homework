67e5f090489e0011c2b7f15dd809cbb02ee3dbd6
vazirabonu [FE16] <102415721+vazirabonu23@users.noreply.github.com>
2022-11-25 15:02:12 +0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,75 @@
+//homework1
+
+// let firstname = "Vazirabonu"
+// let surname = "O'ralova"
+// let age = "16"
+
+// console.log("Mening ismim" + " " +firstname+ ", Familiyasi esa " + surname)
+
+
+//homewoork2
+
+// let obj = {
+//     firstname: "Vazirabonu",
+//     surname:"O'ralova",
+//     age:16,
+//     color:"green",
+// }
+// console.log(obj);
+
+
+
+// homework3
+
+// let one = prompt("Birinch sonni kiriting")
+// let two= prompt("Ikkinchi sonni kiriting")
+
+// console.log(Number(one) % Number(two))
+
+
+
+//homework-4
+
+// let obj ={
+//     name : prompt('Ismingizni kiriting ?'),
+//     surname : prompt('Fammiliyangizni kiriting ?'),
+//     age : prompt('Yoshingizni kiriting ?')
+// };
+// console.log(obj);
+
+
+//homework-5
+ 
+// if(age <= 7){
+//     console.log("Bog'chaga boradi");
+// }
+// else if(age <= 16){
+//     console.log('Maktabda uqiydi');
+// }
+// else if(age == 18){
+//     console.log('Maktabni tugatgan');
+// }
+// else{
+//     console.log('U universitetda uqiydi');
+// }
+
+
+
+//homework -6
+
+// let color = prompt("Svitafor rangini kiriting")
+
+// if(color == "qizil"){
+//     console.log("sariq");
+// }
+// else if(color == "sariq"){
+//     console.log("yashil");
+// }
+// else if(color == "yashil"){
+//     console.log("qizil");
+// }
+// else{
+//     console.log("Siz svitafor rangini kiritmadingiz");
+// }
+
+
